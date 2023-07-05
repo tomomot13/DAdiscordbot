299c9397a81ca0f1a57a970a0a347bfe848a3121
tomomot13 <tomomot13@users.noreply.github.com>
2023-07-05 17:12:54 -0400

    First Discord Bot Lesson Commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.DirectMessages
+        
+    ]
+})
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online.`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+    console.log(message.content)
+})
+
+client.login(process.env.TOKEN);
+
